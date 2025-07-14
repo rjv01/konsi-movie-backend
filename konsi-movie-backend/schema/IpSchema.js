116c7bef45cade79d3de3cd1972c074e00e23085
rjv01 <verma.rajshekhar1@gmail.com>
2025-07-14 11:46:10 +0530

    adding limiter1

--- konsi-movie-backend/schema/IpSchema.js
+++ konsi-movie-backend/schema/IpSchema.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const IpSchema = new mongoose.Schema({
-    ipaddress : {
-        type:String
-    },
-},{timestamps:true});
+  ipaddress: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
 
-const IpModel = mongoose.model("IpModel",IpSchema);
+const IpModel = mongoose.model("IpModel", IpSchema);
 
 module.exports = IpModel;
